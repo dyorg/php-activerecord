36d7faa71ab7d90e8429580556f164a14af7152d
Dyorg Almeida <dyorg.almeida@gmail.com>
2019-04-22 19:58:35 -0300

    Bugfix query_column_info for postgresql

--- lib/adapters/PgsqlAdapter.php
+++ lib/adapters/PgsqlAdapter.php
@@ -60,31 +60,31 @@ class PgsqlAdapter extends Connection
 
 	public function query_column_info($table)
 	{
-		$sql = <<<SQL
-SELECT
-      a.attname AS field,
-      a.attlen,
-      REPLACE(pg_catalog.format_type(a.atttypid, a.atttypmod), 'character varying', 'varchar') AS type,
-      a.attnotnull AS not_nullable,
-      (SELECT 't'
-        FROM pg_index
-        WHERE c.oid = pg_index.indrelid
-        AND a.attnum = ANY (pg_index.indkey)
-        AND pg_index.indisprimary = 't'
-      ) IS NOT NULL AS pk,      
-      REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE((SELECT pg_attrdef.adsrc
-        FROM pg_attrdef
-        WHERE c.oid = pg_attrdef.adrelid
-        AND pg_attrdef.adnum=a.attnum
-      ),'::[a-z_ ]+',''),'''$',''),'^''','') AS default
-	  FROM pg_attribute a, pg_class c, pg_type t, pg_namespace n
-	  WHERE n.nspname = ? 
-	  AND c.relname = ?
-      AND a.attnum > 0
-      AND a.attrelid = c.oid
-      AND a.atttypid = t.oid
-	  ORDER BY a.attnum
-SQL;
+		$sql = "SELECT
+      		a.attname AS field,
+      		a.attlen,
+      		REPLACE(pg_catalog.format_type(a.atttypid, a.atttypmod), 'character varying', 'varchar') AS type,
+      		a.attnotnull AS not_nullable,
+      		(SELECT 't'
+        	FROM pg_index
+        	WHERE c.oid = pg_index.indrelid
+        	AND a.attnum = ANY (pg_index.indkey)
+        	AND pg_index.indisprimary = 't'
+      		) IS NOT NULL AS pk,      
+      		REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE((SELECT pg_attrdef.adsrc
+        	FROM pg_attrdef
+        	WHERE c.oid = pg_attrdef.adrelid
+        	AND pg_attrdef.adnum=a.attnum
+      		),'::[a-z_ ]+',''),'''$',''),'^''','') AS default
+	  		FROM pg_attribute a
+			JOIN pg_class c ON c.oid = a.attrelid
+			JOIN pg_type t on t.oid = a.atttypid
+			JOIN pg_namespace n ON n.oid = c.relnamespace
+			WHERE n.nspname = ? 
+			AND c.relname = ?
+			AND a.attnum > 0
+			ORDER BY a.attnum
+			";
 
         $values = explode('.', $table);
 		return parent::query($sql,$values);
